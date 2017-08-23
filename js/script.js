2ef57a234a45717bc4a1bc5a35c83b5b83469322
Eric Johnson <ekj02451@gmail.com>
2017-08-23 12:03:57 -0500

    Added JS function to insert background image on search

--- js/script.js
+++ js/script.js
@@ -12,6 +12,15 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
+
+    $greeting.text('So, you want to live at ' + address + '?');
+
+    // var streetviewUrl = 'http://imgsv.imaging.nikon.com/lineup/lens/zoom/normalzoom/af-s_dx_18-140mmf_35-56g_ed_vr/img/sample/img_01.jpg';
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + address;
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
     // YOUR CODE GOES HERE!
 
